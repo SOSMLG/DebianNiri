36b6e79ba76739fec721a6263ed865e457cba305
SOSMLG <117748270+SOSMLG@users.noreply.github.com>
2026-01-16 15:31:56 +0100

    Create Debianniri.sh

---
+++ Debianniri.sh
@@ -0,0 +1,5 @@
+curl -fsSL https://download.opensuse.org/repositories/home:AvengeMedia:danklinux/Debian_13/Release.key | \
+  sudo gpg --dearmor -o /etc/apt/keyrings/danklinux.gpg
+echo "deb [signed-by=/etc/apt/keyrings/danklinux.gpg] https://download.opensuse.org/repositories/home:/AvengeMedia:/danklinux/Debian_13/ /" | \
+  sudo tee /etc/apt/sources.list.d/danklinux.list
+sudo apt update
