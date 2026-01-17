b48c4e119a41e89ed4737796d5351a992dc3ae70
SOSMLG <117748270+SOSMLG@users.noreply.github.com>
2026-01-17 12:09:34 +0100

    Update DebianNiri.sh

--- scripts/DebianNiri.sh
+++ scripts/DebianNiri.sh
@@ -1,9 +1,13 @@
-# Add DankLinux repository
+# DankLinux repository
 curl -fsSL https://download.opensuse.org/repositories/home:AvengeMedia:danklinux/Debian_13/Release.key | \
   sudo gpg --dearmor -o /etc/apt/keyrings/danklinux.gpg
 echo "deb [signed-by=/etc/apt/keyrings/danklinux.gpg] https://download.opensuse.org/repositories/home:/AvengeMedia:/danklinux/Debian_13/ /" | \
   sudo tee /etc/apt/sources.list.d/danklinux.list
-sudo apt update
 
+# DMS stable repository
+curl -fsSL https://download.opensuse.org/repositories/home:/AvengeMedia:/dms/Debian_13/Release.key | \
+  sudo gpg --dearmor -o /etc/apt/keyrings/avengemedia-dms.gpg
+echo "deb [signed-by=/etc/apt/keyrings/avengemedia-dms.gpg] https://download.opensuse.org/repositories/home:/AvengeMedia:/dms/Debian_13/ /" | \
+  sudo tee /etc/apt/sources.list.d/avengemedia-dms.list
 # Install stable packages
-sudo apt install quickshell niri
+sudo apt install quickshell niri dms
